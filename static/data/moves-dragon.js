5dfca3e55ad51fd111f2eae70631ff27b9a2ffa0
Daniel.Huang <Daniel.Huang@infortrend.com>
2021-04-20 18:19:31 +0800

    20210420: DragonMove: sketch

--- static/data/moves-dragon.js
+++ static/data/moves-dragon.js
@@ -3,14 +3,234 @@ if(!MoveList) MoveList = [];
 
 Array.prototype.push.apply(MoveList, [
 	{
-		"name": "",
+		"name": "廣域破壞", "alias": "ワイドブレイカー|Breaking Swipe",
 		"power": "",
 		"category": "",
 		"type": "Dragon",
-		"tags": ["target|l|foe"],
+		"tags": ["target|l|"],
 		"accuracy": " + ",
-		"damage": " +　",
+		"damage": "",
 		"effect": "",
 		"desc": ""
 	},
+	{
+		"name": "鱗片噪音", "alias": "スケイルノイズ|Clanging Scales",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "魂舞烈音爆", "alias": "ソウルビート|Clangorous Soul",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "核心懲罰者", "alias": "コアパニッシャー|Core Enforcer",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "流星群", "alias": "龍星群|りゅうせいぐん|Draco Meteor",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "龍息", "alias": "りゅうのいぶき|Dragon Breath",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "龍爪", "alias": "ドラゴンクロー|Dragon Claw",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "龍之舞", "alias": "りゅうのまい|Dragon Dance",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "龍箭", "alias": "ドラゴンアロー|Dragon Darts",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "龍錘", "alias": "ドラゴンハンマー|Dragon Hammer",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "龍之波動", "alias": "りゅうのはどう|Dragon Pulse",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "龍之怒", "alias": "りゅうのいかり|Dragon Rage",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "龍之俯衝", "alias": "ドラゴンダイブ|Dragon Rush",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "龍尾", "alias": "ドラゴンテール|Dragon Tail",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "二連劈", "alias": "ダブルチョップ|Dual Chop",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "極巨炮", "alias": "ダイマックスほう|Dynamax Cannon",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "無極光束", "alias": "ムゲンダイビーム|Eternabeam",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "逆鱗", "alias": "げきりん|Outrage",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "時光咆哮", "alias": "ときのほうこう|Roar of Time",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "亞空裂斬", "alias": "あくうせつだん|Spacial Rend",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	},
+	{
+		"name": "龍捲風", "alias": "たつまき|Twister",
+		"power": "",
+		"category": "",
+		"type": "Dragon",
+		"tags": ["target|l|"],
+		"accuracy": " + ",
+		"damage": "",
+		"effect": "",
+		"desc": ""
+	}
 ]);
